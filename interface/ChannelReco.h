23ac7e0349887f17e16825adeefac35bcbc904fb
Jordan <Jordan.Damgov@cern.ch>
2015-07-22 13:33:22 -0500

    synced with FastTimeTestBeamAnalysis

---
+++ interface/ChannelReco.h
@@ -0,0 +1,77 @@
+#ifndef _varplot_h_
+#define _varplot_h_
+
+#include "TObject.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TGraph.h"
+#include "TString.h"
+
+#include "interface/WaveformUtils.hpp"
+#include "interface/WaveformFit.hpp"
+#include "interface/JSONWrapper.h"
+
+class ChannelReco{
+
+public:
+
+  enum PlotType { kNull, kPlot1D, kPlot1DProf, kPlot2D, kPlot2DProf, kPlotGraph, kPlotHistory };
+
+  ChannelReco(JSONWrapper::Object &cfg, PlotType ptype, Bool_t doPlot, Bool_t doProfile);
+ 
+  UInt_t Size()                                               { return x_.size(); }
+  std::vector<float> &GetX()                                  { return x_; }
+  float GetX(UInt_t i)                                        { return x_.at(i); }
+  std::vector<float> &GetY()                                  { return y_; }
+  float GetY(UInt_t i)                                        { return y_.at(i); }
+  std::pair<float,float> GetXY(uint i)                        { return std::pair<float,float>(x_.at(i),y_.at(i)); }
+  std::pair<std::vector<float> *, std::vector<float>*> Get2D(){ return std::pair<std::vector<float>*, std::vector<float>*>(&x_,&y_); }
+  void ClearVectors()                                         { x_.clear(); y_.clear(); }
+  void SetPlot(TObject* plot)                                 { plot_=plot; }
+  TObject* GetPlot()                                          { return plot_; }
+  TString GetName()                                           { return name_; }
+  //Int
+  Int_t GetGroup()                                            { return group_; }
+  Int_t GetModule()                                           { return module_; }
+  Int_t GetThrForPulseInversion()                             { return thrForPulseInversion_; }
+  Int_t GetPedestalWindowLo()                                 { return pedestalWindowLo_; }
+  Int_t GetPedestalWindowUp()                                 { return pedestalWindowUp_; }
+  Int_t GetSearchWindowUp()                                   { return searchWindowUp_; }
+  Int_t GetSearchWindowLo()                                   { return searchWindowLo_; }  
+  Int_t GetSpyWindowLo()                                      { return spyWindowLo_; }
+  Int_t GetSpyWindowUp()                                      { return spyWindowUp_; }
+  Int_t GetSamplesToInterpolateAtMax()                        { return samplesToInterpolateAtMax_; } 
+  Int_t GetSamplesToInterpolateForCFD()                       { return samplesToInterpolateForCFD_; } 
+  Int_t GetSamplesToInterpolateForTD()                       { return samplesToInterpolateForTD_; } 
+  //Float
+  Float_t GetCFDWindowLo()                                    { return cfdWindowLo_; }
+  Float_t GetSearchWindowAfterLo()                            { return searchWindowAfterLo_; }
+  Float_t GetSearchWindowAfterUp()                            { return searchWindowAfterUp_; }
+  Float_t GetThrForTiming()                                   { return thrForTiming_; }
+  Waveform *GetWaveform()                                     { return waveform_; }
+  TH1F *GetConfigSummary()                                    { return chRecoCfgH_; }
+  void SetWaveform(Waveform *w)                               { waveform_=w; }
+  void Fill(float valX, float valY, int i=-1);
+  void Fill2D(float valX, float valY, float valZ, int i);
+  void Print();
+  
+  ~ChannelReco();
+
+ private:
+  
+  Int_t group_, module_,searchWindowUp_,searchWindowLo_,spyWindowLo_,spyWindowUp_;
+  Float_t cfdWindowLo_;
+  Float_t searchWindowAfterLo_, searchWindowAfterUp_;
+  Int_t thrForPulseInversion_,pedestalWindowLo_,pedestalWindowUp_;
+  Int_t samplesToInterpolateAtMax_,samplesToInterpolateForCFD_,samplesToInterpolateForTD_;
+  Float_t thrForTiming_;
+  int ptype_;
+  bool doPlot_, doProfile_;
+  TString name_;
+  std::vector<float> x_,y_,z_;
+  TH1F *chRecoCfgH_;
+  TObject *plot_;
+  Waveform *waveform_;
+};
+
+#endif
---
+++ src/ChannelReco.cc
@@ -0,0 +1,175 @@
+#include "interface/ChannelReco.h"
+#include <iostream>
+//
+ChannelReco::ChannelReco(JSONWrapper::Object &cfg, PlotType ptype, Bool_t doPlot, Bool_t doProfile) :
+  ptype_(ptype),
+  doPlot_(doPlot),
+  doProfile_(doProfile),
+  plot_(0),
+  waveform_(0)
+{
+  //read configuration
+  name_              = cfg["name"].toString();
+  group_             = cfg["group"].toInt();
+  module_            = cfg["channel"].toInt();
+  searchWindowUp_       = cfg["searchWindowUp"].toInt();
+  searchWindowLo_       = cfg["searchWindowLo"].toInt();
+  searchWindowAfterUp_       = cfg["searchWindowAfterUp"].toDouble();
+  searchWindowAfterLo_       = cfg["searchWindowAfterLo"].toDouble();
+  spyWindowUp_       = cfg["spyWindowUp"].toInt();
+  spyWindowLo_       = cfg["spyWindowLo"].toInt();
+  cfdWindowLo_       = cfg["cfdWindowLo"].toDouble();
+  thrForTiming_ = cfg["thrForTiming"].toDouble();
+  pedestalWindowLo_   = cfg["pedestalWindowLo"].toInt();
+  pedestalWindowUp_   = cfg["pedestalWindowUp"].toInt();
+  thrForPulseInversion_ = cfg["thrForPulseInversion"].toInt();
+  samplesToInterpolateAtMax_ = cfg["samplesToInterpolateAtMax"].toInt();
+  samplesToInterpolateForCFD_ = cfg["samplesToInterpolateForCFD"].toInt();
+  samplesToInterpolateForTD_ = cfg["samplesToInterpolateForTD"].toInt();
+  //save configuration values in a histogram
+  chRecoCfgH_ = new TH1F(name_+"_cfg",";"+name_+";Value",17,0,17);
+  chRecoCfgH_->SetDirectory(0);
+  chRecoCfgH_->SetBinContent(1,group_);                  chRecoCfgH_->GetXaxis()->SetBinLabel(1,"group");
+  chRecoCfgH_->SetBinContent(2,module_);                 chRecoCfgH_->GetXaxis()->SetBinLabel(2,"channel");
+  chRecoCfgH_->SetBinContent(3,searchWindowUp_);         chRecoCfgH_->GetXaxis()->SetBinLabel(3,"searchWindowUp");
+  chRecoCfgH_->SetBinContent(4,searchWindowLo_);         chRecoCfgH_->GetXaxis()->SetBinLabel(4,"searchWindowLo");
+  chRecoCfgH_->SetBinContent(5,searchWindowAfterUp_);         chRecoCfgH_->GetXaxis()->SetBinLabel(5,"searchWindowAfterUp");
+  chRecoCfgH_->SetBinContent(6,searchWindowAfterLo_);         chRecoCfgH_->GetXaxis()->SetBinLabel(6,"searchWindowAfterLo");
+  chRecoCfgH_->SetBinContent(7,cfdWindowLo_);            chRecoCfgH_->GetXaxis()->SetBinLabel(7,"cfdWindowLo");
+  chRecoCfgH_->SetBinContent(8,thrForTiming_);           chRecoCfgH_->GetXaxis()->SetBinLabel(8,"thrForTiming");
+  chRecoCfgH_->SetBinContent(9,pedestalWindowLo_);       chRecoCfgH_->GetXaxis()->SetBinLabel(9,"pedestalWindowLo");
+  chRecoCfgH_->SetBinContent(10,pedestalWindowUp_);       chRecoCfgH_->GetXaxis()->SetBinLabel(10,"pedestalWindowUp");
+  chRecoCfgH_->SetBinContent(11,thrForPulseInversion_);   chRecoCfgH_->GetXaxis()->SetBinLabel(11,"thrForPulseInversion");
+  chRecoCfgH_->SetBinContent(12,spyWindowUp_);           chRecoCfgH_->GetXaxis()->SetBinLabel(12,"spyWindowUp");
+  chRecoCfgH_->SetBinContent(13,spyWindowLo_);           chRecoCfgH_->GetXaxis()->SetBinLabel(13,"spyWindowLo");
+  chRecoCfgH_->SetBinContent(14,samplesToInterpolateAtMax_);       chRecoCfgH_->GetXaxis()->SetBinLabel(14,"samplesToInterpolateAtMax");
+  chRecoCfgH_->SetBinContent(15,samplesToInterpolateForCFD_);       chRecoCfgH_->GetXaxis()->SetBinLabel(15,"samplesToInterpolateForMax");
+  chRecoCfgH_->SetBinContent(16,samplesToInterpolateForTD_);       chRecoCfgH_->GetXaxis()->SetBinLabel(16,"samplesToInterpolateForTD");
+
+  //  std::cout << "\t " << name_ << " will be reconstructed from group=" << group_ << " channel=" << module_ << std::endl;
+  Print();
+}
+
+void ChannelReco::Print()
+{
+  std::cout << "=====  Dump Channel Config  =====" << std::endl;
+  std::cout << "name\t" << name_ << std::endl;
+  std::cout << "group\t" << group_ << std::endl;
+  std::cout << "channel\t" << module_ << std::endl;
+  std::cout << "Pedestal WindowLo\t" << this->GetPedestalWindowLo() << std::endl;
+  std::cout << "Pedestal WindowUp\t" << this->GetPedestalWindowUp() << std::endl;
+  std::cout << "Inversion Threshold\t" << this->GetThrForPulseInversion() << std::endl;
+  std::cout << "Max Search WindowLo\t" << this->GetSearchWindowLo() << std::endl;
+  std::cout << "Max Search WindowUp\t" << this->GetSearchWindowUp() << std::endl;
+  std::cout << "Max Search WindowAfterLo\t" << this->GetSearchWindowAfterLo() << std::endl;
+  std::cout << "Max Search WindowAfterUp\t" << this->GetSearchWindowAfterUp() << std::endl;
+  std::cout << "SaveWaveform WindowLo\t" << this->GetSpyWindowLo() << std::endl;
+  std::cout << "SaveWaveform WindowUp\t" << this->GetSpyWindowUp() << std::endl;
+  std::cout << "CFD WindowLo\t" << this->GetCFDWindowLo() << std::endl;
+  std::cout << "TD Value\t" << this->GetThrForTiming() << std::endl;
+  std::cout << "Samples to interpolate at MAX\t" << this->GetSamplesToInterpolateAtMax() << std::endl;
+  std::cout << "Samples to interpolate for CFD\t" << this->GetSamplesToInterpolateForCFD() << std::endl;
+  std::cout << "Samples to interpolate for TD\t" << this->GetSamplesToInterpolateForTD() << std::endl;
+}
+//
+void ChannelReco::Fill(float valX, float valY, int i) 
+{
+  if(plot_==0 || !doPlot_)
+    {
+      return;
+    }
+  if (ptype_!=kPlot1D && ptype_!=kPlotGraph && ptype_!=kPlot1DProf) 
+    {
+      std::cout << "[ChannelReco::Fill] caught wrong pptype_ in " << name_.Data() << std::endl; 
+      return;
+    }
+
+  if (ptype_==kPlot1D)
+    {
+      if (i<0) x_.push_back(valX);
+      else 
+	{
+	  if (x_.size()<=i) x_.resize(i+1);
+	  x_.at(i)=valX;
+	}
+      (dynamic_cast<TH1F*>(plot_))->Fill(valX);
+    }
+  if (ptype_==kPlot1DProf)
+    {
+      if (i<0) { x_.push_back(valX); y_.push_back(valY); }
+      else
+	{
+	  if (x_.size()<=i)
+	    {
+	      x_.resize(i+1);
+	      y_.resize(i+1);
+	    }
+	  x_.at(i)=valX;
+	  y_.at(i)=valY;
+	}
+      (dynamic_cast<TProfile*>(plot_))->Fill(valX,valY);
+    }
+  if (ptype_==kPlotGraph)
+    {
+      (dynamic_cast<TGraph*>(plot_))->SetPoint(group_,module_,valX);
+    }
+}
+
+//
+void ChannelReco::Fill2D(float valX, float valY, float valZ, int i) 
+{
+  if(plot_==0 || !doPlot_)
+    {
+      return;
+    }
+  if (ptype_!=kPlot2D && ptype_!=kPlot2DProf) 
+    {
+      std::cout << "[ChannelReco::Fill2D] caught wrong pptype_ in " << name_.Data() << std::endl; 
+      return;
+    }
+
+  if (ptype_==kPlot2D)
+    {
+      if (i<0) {x_.push_back(valX); y_.push_back(valY);}
+      else
+	{
+	  if (x_.size()<=i) 
+	    {
+	      x_.resize(i+1);
+	      y_.resize(i+1);
+	    }
+	  x_.at(i)=valX;
+	  y_.at(i)=valY;
+	}
+      (dynamic_cast<TH2F*>(plot_))->Fill(valX,valY);
+    }
+  if (ptype_==kPlot2DProf)
+    {
+      if (i<0)
+	{
+	  x_.push_back(valX); 
+	  y_.push_back(valY); 
+	  z_.push_back(valZ);
+	}
+      else {
+	if (x_.size()<=i) 
+	  {
+	    x_.resize(i+1);
+	    y_.resize(i+1);
+	    z_.resize(i+1);
+	  }
+	x_.at(i)=valX;
+	y_.at(i)=valY;
+	z_.at(i)=valZ;
+      }
+      (dynamic_cast<TProfile2D*>(plot_))->Fill(valX,valY,valZ);
+    }
+}
+
+
+//
+ChannelReco::~ChannelReco()
+{
+  if (plot_)     delete plot_;
+  if (waveform_) delete waveform_;
+}
